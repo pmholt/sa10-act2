4c6d8cfb2f241d4e7cd2153017faf58ba5bf6963
Peyton Holt <pmholt@memphis.edu>
2024-02-28 21:08:59 -0600

    made gilded rose module

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,11 @@
-class GildedRose
+module GildedRose
   attr_reader :item
 
-  def initialize(name, days_remaining, quality)
+  def self.for(name, days_remaining, quality)
     @item = klass_for(name).new(quality, days_remaining)
   end
 
-  def klass_for(name)
+  def self.klass_for(name)
     case name
     when "normal"
       Normal
@@ -16,18 +16,6 @@ class GildedRose
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
     end
-  end
-
-  def tick
-    item.tick
-  end
-
-  def quality
-    item.quality
-  end
-
-  def days_remaining
-    item.days_remaining
   end
 
   class Normal
