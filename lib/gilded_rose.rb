4fe8b3c94cccf91b4755a08b9b0b14480838a4ef
Peyton Holt <pmholt@memphis.edu>
2024-02-28 21:11:09 -0600

    made Item class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,13 +18,14 @@ module GildedRose
     end
   end
 
-  class Normal
+  class Item
     attr_reader :quality, :days_remaining
   
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
     end
-  
+
+  class Normal < Item
     def tick
       @days_remaining -= 1
       return if @quality == 0
@@ -34,13 +35,7 @@ module GildedRose
     end
   end
 
-  class Brie
-    attr_reader :quality, :days_remaining
-  
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  
+  class Brie < Item
     def tick
       @days_remaining -= 1
         return if @quality >= 50
@@ -50,24 +45,12 @@ module GildedRose
     end
   end
 
-  class Sulfuras
-    attr_reader :quality, :days_remaining
-  
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  
+  class Sulfuras < Item
     def tick
     end
   end
 
-  class Backstage
-    attr_reader :quality, :days_remaining
-  
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  
+  class Backstage < Item
     def tick
       @days_remaining -= 1
         return if @quality >= 50
