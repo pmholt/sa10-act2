99ee643d2144563f2a2a37dfb2e710bfe7ee4172
Peyton Holt <pmholt@memphis.edu>
2024-02-28 20:50:19 -0600

    Brie class added

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -20,19 +20,13 @@ class GildedRose
     end
 
     def normal_tick
-      @days_remaining -= 1
-      return if @quality == 0
-
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
+      @item = Normal.new(quality, days_remaining)
+      item.tick
     end
 
     def brie_tick
-      @days_remaining -= 1
-      return if @quality >= 50
-      
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
+      @item = Brie.new(quality, days_remaining)
+      item.tick
     end
 
     def sulfuras_tick
@@ -63,5 +57,21 @@ class Normal
 
     @quality -= 1
     @quality -= 1 if @days_remaining <= 0
+  end
+end
+
+class Brie
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
+      return if @quality >= 50
+      
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
   end
 end
